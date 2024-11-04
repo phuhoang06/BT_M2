835286dea6fb5b00c10d0ccbb69c513875d919be
phuhoang.06 <hoangvanphu2006@gmail.com>
2024-11-04 20:52:20 +0700

    add

---
+++ src/infor.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class infor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Name: ");
+        String name = sc.nextLine();
+        System.out.print("Hello : " + name);
+    }
+}
